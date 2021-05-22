50cfa0f13a0dd268c5c2e609e661e7aeea3261ca
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-05-22 18:50:49 +0100

    Removed redundant logging in identity_enforcement.py

--- ai/identity_enforcement.py
+++ ai/identity_enforcement.py
@@ -8,11 +8,10 @@ LOGGER = logging.getLogger('ai')
 
 
 async def enforce_identity(message: discord.Message, message_copy):
+    LOGGER.info(f"{message.author.display_name} :: Assessing identity for enforcement.")
     if identity_enforcable(message.author, channel=message.channel):
-        LOGGER.info(f"{message.author.display_name} :: Identity enforcable. Updating message copy.")
         message_copy.avatar_url = DRONE_AVATAR
         return False
-    LOGGER.info(f"{message.author.display_name} :: User does not have enforcable identity.")
     return False
 
 
