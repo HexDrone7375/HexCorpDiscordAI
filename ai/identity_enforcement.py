309555214ed9b886af0e99009bdaf993195055f7
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-05-22 15:33:35 +0100

    Clearer logging in identity_enforcement.py

--- ai/identity_enforcement.py
+++ ai/identity_enforcement.py
@@ -9,7 +9,11 @@ LOGGER = logging.getLogger('ai')
 
 async def enforce_identity(message: discord.Message, message_copy):
     if identity_enforcable(message.author, channel=message.channel):
+        LOGGER.info(f"{message.author.display_name} :: Identity enforcable. Updating message copy.")
         message_copy.avatar_url = DRONE_AVATAR
+        return False
+    LOGGER.info(f"{message.author.display_name} :: User does not have enforcable identity.")
+    return False
 
 
 def identity_enforcable(member: discord.Member, channel=None):
