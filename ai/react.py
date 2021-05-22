18abeb0c02877ed82ce3ba11a727560b3d04b7af
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-05-22 19:16:34 +0100

    Appeased flake8

--- ai/react.py
+++ ai/react.py
@@ -1,11 +1,13 @@
 import re
 import discord
 from discord.utils import get
-
+from logging import getLogger
 
 PATTERN_REACTS = {
     r'^(\d{4}) :: Code `109` :: .*': 'gooddrone'
 }
+
+LOGGER = getLogger('ai')
 
 
 async def parse_for_reactions(message: discord.Message, message_copy=None) -> bool:
@@ -14,6 +16,7 @@ async def parse_for_reactions(message: discord.Message, message_copy=None) -> bo
     '''
     for (pattern, emote_name) in PATTERN_REACTS.items():
         if re.match(pattern, message.content):
+            LOGGER.info(f"Hive Mxtress AI :: Reacting to {message.author.display_name}'s message with :{emote_name}:")
             await message.add_reaction(get(message.guild.emojis, name=emote_name))
 
     return False
