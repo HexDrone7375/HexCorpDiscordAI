5e8a20c4981bf79b1603e672217d52eccb9a0a5c
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-05-23 12:50:12 +0100

    Verbose logging for stoplights.py

--- ai/stoplights.py
+++ ai/stoplights.py
@@ -2,12 +2,19 @@ import discord
 from discord.utils import get
 from roles import MODERATION
 from resources import CLOCK, TRAFFIC_LIGHTS
+from logging import getLogger
+
+LOGGER = getLogger('ai')
 
 
 async def check_for_stoplights(message: discord.Message, message_copy=None):
     if CLOCK in message.content:
+        LOGGER.info(f"{message.author.display_name} :: Moderators requested via clock emoji.")
         moderator_role = get(message.guild.roles, name=MODERATION)
         await message.channel.send(f"Moderators needed {moderator_role.mention}!")
         return True
     else:
-        return any(traffic_light in message.content for traffic_light in TRAFFIC_LIGHTS)
+        if any(traffic_light in message.content for traffic_light in TRAFFIC_LIGHTS):
+            LOGGER.info(f"{message.author.display_name} :: Traffic light included in message. Halting message processing.")
+            return True
+        return False
